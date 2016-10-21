89953adb9fe13c7fdcff238f212f63665b00fd95
sumedhashyam <shyam@sumedhasoftech.com>
2016-10-21 15:33:31 +0530

    documentation

--- TestApplicaiton/Program.cs
+++ TestApplicaiton/Program.cs
@@ -11,16 +11,18 @@ namespace TestApplicaiton
     {
         static void Main(string[] args)
         {
-            var path = "D:/projects/PushNotification/TestApplicaiton/Alertify Production.p12";//path of .p12 file  
-            IOSPush iosPush = new IOSPush(ApnsServerEnvironment.Production, path, "1234");
+            var path = "path of .p12 file";
+            IOSPush iosPush = new IOSPush(ApnsServerEnvironment.Production/*ApnsServerEnvironment.Sandbox in case of development*/, path, "Password of p12 file");
         
-              var json = "{\"notificationName\":\"Name\",\"notificationSubject\":\"Subject\",\"notificationBody\":\"Body\",\"sendToUsers\":\"ashkot09@gmail.com,shraddhamulay@gmail.com\",\"scheduledDate\":\"Oct-28-2016 8:00:00\",\"notificationTypeID\":7,\"notificationActions\":[{\"NotificationName\":\"Add to reminder\",\"NotificationActionEntityValues\":[{\"NotificationEntityName\":\"Title\",\"NotificationEntityValue\":\"Test\"},{\"NotificationEntityName\":\"DateTime\",\"NotificationEntityValue\":\"Pqr-28-2016 8:00:00\"}]},{\"NotificationName\":\"Add to calendar\",\"NotificationActionEntityValues\":[{\"NotificationEntityName\":\"Title\",\"NotificationEntityValue\":\"Title\"},{\"NotificationEntityName\":\"Location\",\"NotificationEntityValue\":\"Location\"},{\"NotificationEntityName\":\"All day\",\"NotificationEntityValue\":\"N\"},{\"NotificationEntityName\":\"Start DateTime\",\"NotificationEntityValue\":\"Oct-30-2016 8:00:00\"},{\"NotificationEntityName\":\"End DateTime\",\"NotificationEntityValue\":\"Oct-30-2016 17:00:00\"},{\"NotificationEntityName\":\"Alert\",\"NotificationEntityValue\":\"None\"},{\"NotificationEntityName\":\"Show As\",\"NotificationEntityValue\":\"None\"},{\"NotificationEntityName\":\"Note\",\"NotificationEntityValue\":\"Some notes here\"}]}]}";
-             iosPush.SendWithJSon("53ae9aa1bcb790027c5ca816a2e9648d04ceb2e1d9dfae4551d52ba4414b310a", "Test push send by Shyam Agarwal", json,new Action<Result>(x => {
+              var json = "{\"ID\":\"121\",\"Name\":\"Shyam\",\"notificationActions\":[{\"NotificationName\":\"Add to reminder\",\"NotificationActionEntityValues\":[{\"NotificationEntityName\":\"Title\",\"NotificationEntityValue\":\"Test\"},{\"NotificationEntityName\":\"DateTime\",\"NotificationEntityValue\":\"Pqr-28-2016 8:00:00\"}]},{\"NotificationName\":\"Add to calendar\",\"NotificationActionEntityValues\":[{\"NotificationEntityName\":\"Title\",\"NotificationEntityValue\":\"Title\"},{\"NotificationEntityName\":\"Location\",\"NotificationEntityValue\":\"Location\"},{\"NotificationEntityName\":\"All day\",\"NotificationEntityValue\":\"N\"},{\"NotificationEntityName\":\"Start DateTime\",\"NotificationEntityValue\":\"Oct-30-2016 8:00:00\"},{\"NotificationEntityName\":\"End DateTime\",\"NotificationEntityValue\":\"Oct-30-2016 17:00:00\"},{\"NotificationEntityName\":\"Alert\",\"NotificationEntityValue\":\"None\"},{\"NotificationEntityName\":\"Show As\",\"NotificationEntityValue\":\"None\"},{\"NotificationEntityName\":\"Note\",\"NotificationEntityValue\":\"Some notes here\"}]}]}";
+             //Regular push notification: 4KB (4096 bytes) based on HTTP/2-based APNs provider API
+			 iosPush.SendWithJSon("device token", "Test push send by Shyam Agarwal", json,new Action<Result>(x => {
                  Console.WriteLine(x.status);
              }));
-            // iosPush.SendWithJSon("3cd8e888c3f7f75b345aa294db083992e79c9059e66772095d6ca04b2a52d455", "Test push send by Shyam Agarwal", json);
-            //  iosPush.Send("53ae9aa1bcb790027c5ca816a2e9648d04ceb2e1d9dfae4551d52ba4414b310a", "Test push send by Shyam Agarwal", "ID=121;Name=Shyam");
-            //  iosPush.Send("1834a6920b69f56fd122dc0032dce935f1d1071f875cc492b2db5de1ca91291e", "Test push send by Shyam Agarwal", "ID=121;Name=Shyam");
+			 
+			 //Regular push notification: 2KB (2048 bytes) based on Legacy APNs binary interface
+             iosPush.Send("device token", "Test push send by Shyam Agarwal", "ID=121;Name=Shyam");
+            
         }
     }
 }
