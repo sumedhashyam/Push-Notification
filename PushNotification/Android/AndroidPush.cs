1e4433416c08127f6779ced8e9baa932a01520e3
sumedhashyam <shyam@sumedhasoftech.com>
2016-12-27 15:17:38 +0530

    Support for Andorid push

---
+++ PushNotification/Android/AndroidPush.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json.Linq;
+using PushNotification.IOS;
+using PushSharp.Google;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushNotification.Android
+{
+    public class AndroidPush
+    {
+        String SenderID = "";
+        String AuthToken = "";
+
+        public AndroidPush(string senderID,string authToken)
+        {
+            SenderID = senderID;
+            AuthToken = authToken;
+        }
+        /// <summary>
+        /// Send push message with custom json.
+        /// </summary>
+        /// <param name="userGCMKey">GCM registration key</param>
+        /// <param name="collapseKey">Unique Id that identify the push desination page.</param>
+        /// <param name="json">JSON for extra fields including message</param>
+        /// <param name="callback">callback function</param>
+        public void SendWithJSon(string userGCMKey, string collapseKey,string message, string json, Action<Result> callback)
+        {
+            try
+            {
+                var config = new GcmConfiguration(SenderID, AuthToken, null);
+                var broker = new GcmServiceBroker(config);
+                broker.OnNotificationFailed += (notification, exception) => {
+                    callback(new Result { status = "FAIL", message = exception.Message });
+                };
+                broker.OnNotificationSucceeded += (notification) => {
+
+                    callback(new Result { status = "Success", message = "" });
+                };
+
+                broker.Start();
+                broker.QueueNotification(new GcmNotification
+                {
+                    RegistrationIds = new List<string> {
+                        userGCMKey
+                    },
+                    Data = JObject.Parse("{\"message\":\"" + message + "\",\"payload\":" + json + "}"),
+                    CollapseKey = collapseKey,
+                    TimeToLive = 108,
+                    DelayWhileIdle = true
+                });
+                broker.Stop();
+        }
+            catch (Exception ex)
+            {
+
+                callback(new Result { status = "FAIL", message = ex.Message });
+            }
+        }
+    }
+}
--- TestApplicaiton/Program.cs
+++ TestApplicaiton/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PushNotification.IOS;
+using PushNotification.Android;
 
 namespace TestApplicaiton
 {
@@ -11,6 +12,7 @@ namespace TestApplicaiton
     {
         static void Main(string[] args)
         {
+            /* IOS push example */
             var path = "path of .p12 file";
             IOSPush iosPush = new IOSPush(ApnsServerEnvironment.Production/*ApnsServerEnvironment.Sandbox in case of development*/, path, "Password of p12 file");
 
@@ -23,6 +25,14 @@ namespace TestApplicaiton
             //Regular push notification: 2KB (2048 bytes) based on Legacy APNs binary interface
             iosPush.Send("device token", "Test push send by Shyam Agarwal", "ID=121;Name=Shyam");
 
+            /* Android push example*/
+            AndroidPush androidPush = new AndroidPush("1088893957686", "AIzaSyCkFaqgI5BP2YEpYMpEk5fX1VF0qJX-PPo");
+
+            var json1 = "{\"notificationName\":\"Name\",\"notificationSubject\":\"Notification Test by AK \",\"notificationBody\":\"Body\",\"sendToUsers\":\"ashkot09@gmail.com\",\"notificationTypeID\":1,\"notificationActions\":[{\"NotificationName\":\"Add to reminder\",\"NotificationActionEntityValues\":[{\"NotificationEntityName\":\"Title\",\"NotificationEntityValue\":\"Test\"},{\"NotificationEntityName\":\"DateTime\",\"NotificationEntityValue\":\"Dec/28/2016 15:00:00\"}]},{\"NotificationName\":\"Add to calendar\",\"NotificationActionEntityValues\":[{\"NotificationEntityName\":\"Title\",\"NotificationEntityValue\":\"Title\"},{\"NotificationEntityName\":\"Location\",\"NotificationEntityValue\":\"Location\"},{\"NotificationEntityName\":\"All day\",\"NotificationEntityValue\":\"N\"},{\"NotificationEntityName\":\"Start DateTime\",\"NotificationEntityValue\":\"Dec/28/2016 08:00:00\"},{\"NotificationEntityName\":\"End DateTime\",\"NotificationEntityValue\":\"Dec/10/2017 5:00:00 PM\"},{\"NotificationEntityName\":\"Alert\",\"NotificationEntityValue\":\"None\"},{\"NotificationEntityName\":\"Show As\",\"NotificationEntityValue\":\"\"},{\"NotificationEntityName\":\"Note\",\"NotificationEntityValue\":\"Some notes here\"}]}]}";
+
+            androidPush.SendWithJSon("APA91bEfjIV0Bvdj11ta6HlRMJNsAcOG99uGm9vR1h61ZUf2SF4FxCkF7XLksCFgW_OEU-2H00DwHqrl2txEWSmfSDZaTsibste1Ff8X_6PEEbPnDIAeOQPtGPmLvVeU_Jj0nAPjPkc-", "PAGE 1", "Test push send by Shyam Agarwal", json1, new Action<Result>(x => {
+                Console.WriteLine(x.status);
+            }));
         }
     }
 }
